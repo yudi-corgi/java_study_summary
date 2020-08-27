58ad3dfaba712f87c3bdf5f3236586ae930c6ff4
电动马达臀 <1019509861@qq.com>
2020-08-28 00:59:40 +0800

    add customer Collector implements to partition primes

--- src/com/jdk8learn/streamLearn/PracticeLambda2.java
+++ src/com/jdk8learn/streamLearn/PracticeLambda2.java
@@ -4,39 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class PracticeLambda2 {
 
-    /**
-     * 判断数值是否为质数
-     * @param candidate 数值
-     * @return true 是 false 否
-     */
-    public static boolean isPrime(int candidate){
-        // 只判断小于等于参数的平方根的因子
-        int candidateRoot = (int)Math.sqrt((double)candidate);
-        return IntStream.rangeClosed(2,candidateRoot).noneMatch(n -> candidate % n == 0);
-        // 全部判断
-        // return IntStream.rangeClosed(2,candidate).noneMatch(n -> n % candidate == 0);
-    }
-
-    public static Map<Boolean, List<Integer>> partitionPrimes(int n){
-        return IntStream.rangeClosed(2,100)
-                .boxed().collect(Collectors.partitioningBy(PracticeLambda2::isPrime));
-    }
-
     public static void main(String[] args) {
         inter inter = (a,b)->a+b;
         System.out.println(inter.operation(3,2));
         System.out.println("Hello World!");
-
-        // 根据传入数值n，将前 N 个数分为质数和非质数(包含 n)
-        Map<Boolean, List<Integer>> primeMap = PracticeLambda2.partitionPrimes(100);
-        System.out.println(primeMap);
 
         //调用 Collector 收集器接口实现类
         List<String> list = Arrays.asList("abc","asd","qwe","vbn","fgh","rty");
@@ -45,5 +21,53 @@ public class PracticeLambda2 {
         //直接自定义收集器
         List<String> upperList2 = list.stream().map(String::toUpperCase).collect(ArrayList::new,List::add, List::addAll);
         System.out.println(upperList2);
+
+        long faster = Long.MAX_VALUE;
+        for(int i=0;i<10;i++){
+            long start = System.nanoTime();
+            PracticeLambda2.partitionPrimes(1000000);
+            long duration = (System.nanoTime() - start) / 1000000;
+            if(duration < faster){
+                faster = duration;
+            }
+        }
+        System.out.println("使用预定义收集器 partitioningBy 收集时间:"+faster);
+        long faster2 = Long.MAX_VALUE;
+        for(int i=0;i<10;i++){
+            long start = System.nanoTime();
+            partitionPrimesWithCustomerCollector(1000000);
+            long duration = (System.nanoTime() - start) / 1000000;
+            if(duration < faster){
+                faster2 = duration;
+            }
+        }
+        System.out.println("使用自定义收集器收集时间:"+faster2);
     }
+
+    /**
+     * 调用自定义的分区质数收集器
+     */
+    public static Map<Boolean,List<Integer>> partitionPrimesWithCustomerCollector(int candidate){
+        return IntStream.rangeClosed(2,candidate).boxed().collect(new PrimeNumbersCollector());
+    }
+
+    /**
+     * 判断数值是否为质数
+     * @param candidate 数值
+     * @return true 是 false 否
+     */
+    public static boolean isPrime(int candidate){
+        // 只判断小于等于参数的平方根的因子
+        int candidateRoot = (int)Math.sqrt(candidate);
+        return IntStream.rangeClosed(2,candidateRoot).noneMatch(n -> candidate % n == 0);
+        // 全部判断
+        // return IntStream.rangeClosed(2,candidate).noneMatch(n -> n % candidate == 0);
+    }
+
+    public static Map<Boolean, List<Integer>> partitionPrimes(int n){
+        return IntStream.rangeClosed(2,n)
+                .boxed().collect(Collectors.partitioningBy(PracticeLambda2::isPrime));
+    }
+
+
 }
---
+++ src/com/jdk8learn/streamLearn/PrimeNumbersCollector.java
@@ -0,0 +1,104 @@
+package com.jdk8learn.streamLearn;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * 自定义收集器 实现质数、非质数的分区
+ * @author YUDI
+ * @date 2020/8/28 0:22
+ */
+public class PrimeNumbersCollector implements Collector<Integer,
+        Map<Boolean, List<Integer>>,Map<Boolean, List<Integer>>> {
+
+    /**
+     * 截取小于待测数的质数列表
+     * @param list 质数列表
+     * @param p   判断函数
+     * @param <A> Integer
+     * @return 小于待测数的质数列表 List
+     */
+    public static <A> List<A> takeWhile(List<A> list, Predicate<A> p){
+        int i = 0;
+        for (A a : list) {
+            if(!p.test(a)){
+                return list.subList(0,i);
+            }
+            i++;
+        }
+        return list;
+    }
+
+    /**
+     * 判断到测数是否为质数
+     * @param primeList 已保存的质数列表
+     * @param candidate 待测试
+     * @return  true 质数 false 非质数
+     */
+    public static boolean isPrime(List<Integer> primeList, int candidate){
+        int candidateRoot = (int) Math.sqrt(candidate);
+        return takeWhile(primeList, i -> i <= candidateRoot).stream().noneMatch(p -> candidate % p == 0);
+    }
+
+    /**
+     * 创建累加器
+     */
+    @Override
+    public Supplier<Map<Boolean, List<Integer>>> supplier() {
+        return () -> new HashMap<Boolean, List<Integer>>(){{
+            put(true,new ArrayList<>());
+            put(false,new ArrayList<>());
+        }};
+    }
+
+    /**
+     * 累加方式
+     */
+    @Override
+    public BiConsumer<Map<Boolean, List<Integer>>, Integer> accumulator() {
+        return (Map<Boolean,List<Integer>> acc, Integer candidate)->{
+            acc.get(isPrime(acc.get(true),candidate)).add(candidate);
+        };
+    }
+
+    /**
+     * 并行操作（实际不行，因为并行需要无序，但该累加器计算是有序的）
+     */
+    @Override
+    public BinaryOperator<Map<Boolean, List<Integer>>> combiner() {
+        return (Map<Boolean, List<Integer>> map1,Map<Boolean, List<Integer>> map2)->{
+            map1.get(true).addAll(map2.get(true));
+            map1.get(false).addAll(map2.get(false));
+            return map1;
+        };
+    }
+
+    /**
+     * 转换：将累加器转换为最终结果（此处累加器类型等于结果类型，调用恒等函数）
+     */
+    @Override
+    public Function<Map<Boolean, List<Integer>>, Map<Boolean, List<Integer>>> finisher() {
+        return Function.identity();
+    }
+
+    /**
+     * 收集器行为，因为有序，所以另外两个不需要设置
+     */
+    @Override
+    public Set<Characteristics> characteristics() {
+        return Collections.unmodifiableSet(EnumSet.of(Characteristics.IDENTITY_FINISH));
+    }
+
+
+
+    /*
+     *  以下为通过 partitioningBy() 分区方式获取质数与非质数
+     */
+
+
+
+
+}
