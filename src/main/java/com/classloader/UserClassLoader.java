05545ea2fd8c421a1030e8bfacf8fe53a09651ba
YUDI-Corgi <1019509861@qq.com>
2022-12-05 23:15:24 +0800

    style: Add comment.

--- src/main/java/com/classloader/UserClassLoader.java
+++ src/main/java/com/classloader/UserClassLoader.java
@@ -26,9 +26,15 @@ public class UserClassLoader extends ClassLoader {
         this.classPath = classPath;
     }
 
+    /**
+     * 重写 ClassLoader 的 findClass 方法，不破坏其双亲委派模式
+     * @param name 类名
+     * @return 加载的 Class 对象
+     */
     @Override
     protected Class<?> findClass(String name) {
 
+        // 1. 读取 class 文件
         String className = classPath + name + CLASS_SUFFIX;
         try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(className));
              ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
@@ -37,7 +43,9 @@ public class UserClassLoader extends ClassLoader {
             if ((len = bis.read(data)) != -1) {
                 baos.write(data, 0, len);
             }
+            // 2. 转为字节数组
             byte[] classBytes = baos.toByteArray();
+            // 3. 调用 ClassLoader#defineClass 将字节数组转为 Class 实例
             return defineClass(null, classBytes, 0, classBytes.length);
         } catch (IOException e) {
             e.printStackTrace();
